6232d6c36290bf5f08e0d34119d63752d7d2ad3e
drakeh945 <drake.herring@gmail.com>
2022-03-25 10:02:21 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/contact-form", (req, res) => {
+  console.log(req.body.email);
+  console.log(req.body.name);
+  res.send("Thank you for submitting you contact form");
+});
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.listen(3000);
